f5b644a57309276d0566449a3f71c486b4a90bf0
nycck <vanbeusichemnick@gmail.com>
2024-11-13 22:12:23 +0100

    14e commit

--- app/views/magazijn/levering_info.php
+++ app/views/magazijn/levering_info.php
@@ -1,34 +1,40 @@
 <?php require_once APPROOT . '/views/includes/header.php'; ?>
 <div class="container">
     <h3><?= $data['title']; ?></h3>
-    <p>Naam Product: <?= $data['product']->Naam; ?></p>
-    <p>Barcode: <?= $data['product']->Barcode; ?></p>
     <?php if (isset($data['message'])): ?>
         <div class="alert alert-<?= $data['messageColor']; ?>" role="alert" style='<?= $data['messageVisibility']; ?>'>
             <?= $data['message']; ?>
         </div>
     <?php endif; ?>
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>Allergene Naam</th>
-                <th>Omschrijving</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php if (empty($data['allergenenData'])): ?>
+    <?php if (!empty($data['leveringData'])): ?>
+        <table class="table table-hover">
+            <thead>
                 <tr>
-                    <td colspan="2" class="text-center">In dit product zitten geen stoffen die een allergische reactie kunnen veroorzaken</td>
+                    <th>Naam Leverancier</th>
+                    <th>Contactpersoon</th>
+                    <th>Leveranciernummer</th>
+                    <th>Mobiel</th>
+                    <th>Leveringsdatum</th>
+                    <th>Verwachte Leveringsdatum</th>
+                    <th>Aantal</th>
                 </tr>
-            <?php else: ?>
-                <?php foreach ($data['allergenenData'] as $allergene): ?>
+            </thead>
+            <tbody>
+                <?php foreach ($data['leveringData'] as $levering): ?>
                     <tr>
-                        <td><?= $allergene->Naam; ?></td>
-                        <td><?= $allergene->Omschrijving; ?></td>
+                        <td><?= $levering->LeverancierNaam; ?></td>
+                        <td><?= $levering->Contactpersoon; ?></td>
+                        <td><?= $levering->Leveranciernummer; ?></td>
+                        <td><?= $levering->Mobiel; ?></td>
+                        <td><?= $levering->LeveringsDatum; ?></td>
+                        <td><?= $levering->VerwachteLeveringsDatum; ?></td>
+                        <td><?= $levering->Aantal; ?></td>
                     </tr>
                 <?php endforeach; ?>
-            <?php endif; ?>
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p class="text-center">Geen leveringsgegevens gevonden</p>
+    <?php endif; ?>
 </div>
 <?php require_once APPROOT . '/views/includes/footer.php'; ?>
