0d299e3584d2574c53ea0c7719e3c31a6aac0279
nycck <vanbeusichemnick@gmail.com>
2024-11-13 20:51:11 +0100

    3e commit

--- app/models/MagazijnModel.php
+++ app/models/MagazijnModel.php
@@ -1,15 +1,16 @@
 <?php
-
 class MagazijnModel
 {
     private $db;
-
     public function __construct()
     {
-        /**
-         * Maak een nieuw database object die verbinding maakt met de 
-         * MySQL server
-         */
         $this->db = new Database();
-    }    
+    }
+
+    public function getLeveringByProductId($productId)
+    {
+        $this->db->query('CALL spGetLeveringByProductId(:productId)');
+        $this->db->bind(':productId', $productId, PDO::PARAM_INT);
+        return $this->db->resultSet();
+    }
 }
